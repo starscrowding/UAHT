b04837c95f0e041cc43f9c2ea3a3093002e412b3
Admin <admin@Admins-MBP.fritz.box>
2022-12-18 12:38:27 +0100

    pause settlepay

--- components/Wallet/constants.tsx
+++ components/Wallet/constants.tsx
@@ -26,12 +26,12 @@ export const PROVIDERS: {
     id: 'bf43e1c8e907ea295c858f042f02411d2279f085b829bdda5ea23596801804d1',
     validator: {test: v => v?.length > 60} as RegExp,
   },
-  settlepay: {
+  /* settlepay: {
     text: 'SettlePay',
     help: 'https://settlepay.net/ua/',
     id: 'SP53950543598bc',
     validator: /^SP.+$/,
-  },
+  }, */
   advcash: {
     text: 'Advcash',
     help: 'https://wallet.advcash.com:443/referral/49cff02e-b8bb-42b0-8f09-97f82838b1c7',

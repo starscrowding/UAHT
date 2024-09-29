f3cab44d3a69f3a159e46753aab46da225d55812
ToDo <ToDo@users.noreply.github.com>
2024-09-29 17:04:28 +0200

    limit order link

--- components/Wallet/swap.component.tsx
+++ components/Wallet/swap.component.tsx
@@ -33,7 +33,7 @@ export const Swap = ({balance, gas}: any) => {
           auto
           onClick={() => {
             window.open(
-              `https://app.1inch.io/#/${POLYGON_NETWORK}/simple/swap/${USDT_ADDRESS}/${ADDRESS}`,
+              `https://app.1inch.io/#/${POLYGON_NETWORK}/advanced/limit-order/${USDT_ADDRESS}/${ADDRESS}`,
               '_blank'
             );
           }}

514954d4047268105ec509c57108bba937b35b0c
starscrowding.com <99181090+starscrowding@users.noreply.github.com>
2024-10-14 12:41:37 +0200

    Update UAHT_JAR.sol
    https://snapshot.org/#/uaht-io.eth/proposal/0xe932abd71359d35a44b70d815d8199921a3756add5e83ab1f65bbcc43ffd8a6d

--- contracts/UAHT_JAR.sol
+++ contracts/UAHT_JAR.sol
@@ -13,27 +13,34 @@ contract UAHT_JAR is ReentrancyGuard {
     address public uaht_oracle = 0x626CEF050c13eDE2B1b801bc6a76863E212ec25d;
 
     struct Position { // позика
-      address sender;
+      address next;
+      address prev;
       address asset;
       uint256 stake;
       uint256 debt;
+      uint256 end;
    }
 
-    mapping (uint256 => Position) public position;
+    address public base;
+    mapping (address => Position) public position;
+    mapping (address => uint256) public stake;
 
-    event Put(uint256 timestamp, uint256 _in, uint256 _out);
-    event Pop(uint256 timestamp, uint256 _out, uint256 _in);
+    event Sync(address from, Position position);
+    event Put(uint256 _in, uint256 _out);
+    event Pop(uint256 _out, uint256 _in);
 
     function value(address asset) public view returns(uint256) {
         return UAHT_ORACLE(uaht_oracle).data(asset, jar);
     }
 
-    function to_uaht(address asset, uint256 amount) public view returns(uint256) {
-        return Math.mulDiv(amount, value(asset), 10 ** ERC20(asset).decimals());
+    function to_uaht(address asset, uint256 amount, uint256 _days) public view returns(uint256) {
+        uint256 rate = 100 - Math.sqrt(_days);
+        require(rate > 0, "!rate");
+        return Math.mulDiv(amount, value(asset), 10 ** ERC20(asset).decimals()) * rate / 100;
     }
 
     function to_asset(address asset, uint256 uaht) public view returns(uint256) {
-        return Math.mulDiv(uaht, 10 ** ERC20(asset).decimals(), value(asset));
+        return Math.mulDiv(uaht, 10 ** ERC20(asset).decimals(), value(asset)) * 99 / 100;
     }
 
     function total_uaht() public view returns(uint256) {
@@ -44,31 +51,111 @@ contract UAHT_JAR is ReentrancyGuard {
         return ERC20(asset).balanceOf(address(this));
     }
 
-    function top() public view returns(uint256) {
+    function max_uaht() public view returns(uint256) {
         return UAHT_DAO(uaht_dao).allowance(address(this));
     }
 
-    function free() public view returns(uint256) {
-        return top() - total_uaht();
+    function free_uaht() public view returns(uint256) {
+        return max_uaht() - total_uaht();
     }
 
-    function put(address asset, uint256 amount) public nonReentrant() returns(uint256) { // застава
-        require(position[block.timestamp].sender == address(0), "!empty");
+    function free_asset(address asset) public view returns(uint256) {
+        return total_asset(asset) - stake[asset];
+    }
+
+    function simulate_free_asset(address asset) public returns(uint256) {
+        sync();
+        return free_asset(asset);
+    }
+
+    function sync() public { // синхронізація
+        address next = base;
+        while(next != address(0) && position[next].end <= block.timestamp) {
+            address _next = position[next].next;
+            sync_position(next);
+            next = _next;
+        }
+    }
+
+    function sync_position(address index) public {
+        if(index != address(0) && position[index].end <= block.timestamp) {
+            stake[position[index].asset] -= position[index].stake;
+            emit Sync(index, position[index]);
+            if(index == base) {
+                base = position[index].next;
+            }
+            if (position[index].prev != address(0)) {
+                position[position[index].prev].next = position[index].next;
+            }
+            if (position[index].next != address(0)) {
+                position[position[index].next].prev = position[index].prev;
+            }
+            delete position[index];
+        }
+    }
+
+    function put(address asset, uint256 amount, uint256 _days) public nonReentrant() returns(uint256) { // застава
         SafeERC20.safeTransferFrom(ERC20(asset), msg.sender, address(this), amount);
-        position[block.timestamp] = Position(msg.sender, asset, amount, to_uaht(asset, amount));
-        require(position[block.timestamp].debt > 0, "!debt");
-        UAHT_DAO(uaht_dao).input(msg.sender, position[block.timestamp].debt);
-        emit Put(block.timestamp, amount, position[block.timestamp].debt);
-        return block.timestamp;
+        if(position[msg.sender].end != 0) {
+            require(position[msg.sender].asset == asset , "!asset");
+            require(position[msg.sender].end > block.timestamp, "!!end");
+            uint256 debt = to_uaht(asset, amount, (position[msg.sender].end - block.timestamp) / 1 days);
+            require(debt > 0, "!debt");
+            position[msg.sender].debt += debt;
+            position[msg.sender].stake += amount;
+            UAHT_DAO(uaht_dao).input(msg.sender, debt);
+            stake[position[msg.sender].asset] += amount;
+            emit Put(amount, debt);
+            return debt;
+        } else {
+            require(position[msg.sender].debt == 0 , "!empty");
+            position[msg.sender] = Position(address(0), address(0), asset, amount, to_uaht(asset, amount, _days), block.timestamp + _days * 1 days);
+            require(position[msg.sender].debt > 0, "!debt");
+            UAHT_DAO(uaht_dao).input(msg.sender, position[msg.sender].debt);
+            stake[position[msg.sender].asset] += position[msg.sender].stake;
+            if (base == address(0)) {
+                base = msg.sender;
+            } else {
+                address next = base;
+                address prev = address(0);
+                while (next != address(0) && position[next].end < position[msg.sender].end) {
+                    prev = next;
+                    next = position[next].next;
+                }
+                position[msg.sender].next = next;
+                position[msg.sender].prev = prev;
+                if (prev != address(0)) {
+                    position[prev].next = msg.sender;
+                } else {
+                    base = msg.sender;
+                }
+                if (next != address(0)) {
+                    position[next].prev = msg.sender;
+                }
+            }
+            emit Put(amount, position[msg.sender].debt);
+            return position[msg.sender].debt;
+        }
     }
 
-    function pop(uint256 timestamp) public nonReentrant() { // ліквідація
-        require(position[timestamp].sender == msg.sender || timestamp + 365 * 24 * 60 * 60 < block.timestamp, "!access");
-        require(position[timestamp].stake > 0 && position[timestamp].debt > 0, "empty");
-        UAHT_DAO(uaht_dao).output(msg.sender, position[timestamp].debt);
-        SafeERC20.safeTransfer(ERC20(position[timestamp].asset), msg.sender, position[timestamp].stake);
-        emit Pop(timestamp, position[timestamp].stake, position[timestamp].debt);
-        position[timestamp].stake = 0;
-        position[timestamp].debt = 0;
+    function pop(address asset, uint256 uaht, address to) public nonReentrant() returns(uint256) { // ліквідація
+        if(position[msg.sender].debt == 0) {
+            sync();
+            uint256 _stake = to_asset(asset, uaht);
+            require(_stake > 0 && _stake < free_asset(asset), "!stake");
+            UAHT_DAO(uaht_dao).output(msg.sender, uaht);
+            SafeERC20.safeTransfer(ERC20(asset), to, _stake);
+            emit Pop(_stake, uaht);
+            return _stake;
+        } else {
+            require(uaht == position[msg.sender].debt, "!position");
+            uint256 _stake = position[msg.sender].stake;
+            UAHT_DAO(uaht_dao).output(msg.sender, position[msg.sender].debt);
+            SafeERC20.safeTransfer(ERC20(position[msg.sender].asset), to, _stake);
+            emit Pop(_stake, position[msg.sender].debt);
+            position[msg.sender].end = 0;     
+            sync_position(msg.sender);
+            return _stake;
+        }
     }
 }

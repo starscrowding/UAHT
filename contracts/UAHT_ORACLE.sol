94261b17e20a6b8f965ce112b4492fa3dfb20fd7
starscrowding.com <99181090+starscrowding@users.noreply.github.com>
2024-10-20 12:58:38 +0200

    Update UAHT_ORACLE.sol

--- contracts/UAHT_ORACLE.sol
+++ contracts/UAHT_ORACLE.sol
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.28;
 
 contract UAHT_ORACLE { // permissionless
+    struct Data {
+      uint256 value;
+      uint256 timestamp;
+   }
+
+    mapping (address => mapping(address => Data)) public feeds; 
+
     function data(address asset, address feed) public view returns(uint256) { // ASSET/UAHT
-        return ERC20(asset).allowance(feed, asset);
+        require(feeds[feed][asset].value > 0, "!data");
+        return feeds[feed][asset].value;
+    }
+
+    function set(address asset, uint256 value) public {
+        feeds[msg.sender][asset] = Data(value, block.timestamp);
     }
 }
